6ef89a332f653e4f5a239162cbcadfa7e00caa35
zbindxen <zbindxen@students.zhaw.ch>
2025-05-26 10:11:58 +0200

    changed dataset name for data report

--- src/main.py
+++ src/main.py
@@ -72,8 +72,7 @@ data_set2_population = pd.read_excel("Data/2_Population_cantons.xlsx")
 data_set3 = pd.read_excel("Data/3_Todesursachen Schweiz ohne Alter 1876-2002.xlsx")
 data_set3_cleaned = pd.read_csv("Data/data_set3_cleaned.csv")
 data_covid = pd.read_csv("Data/full_data.csv")
-data = pd.read_csv("Data/data_set3_cleaned.csv")
-infectdata = pd.read_csv("Data/cleaned_infectious_diseases.csv")
+dataset3_infectdata = pd.read_csv("Data/dataset_3_cleaned_infectious_diseases.csv")
 
 
 # Tabs erstellen
@@ -183,20 +182,20 @@ with tab3:
     st.header("Dataset 3: Causes of Death Over Time")
 
     st.subheader("1. Major Death Causes Over Time")
-    fig3_1 = plot_major_causes_over_time(data)
+    fig3_1 = plot_major_causes_over_time(data_set3_cleaned)
     with st.container():
         streamlit_bokeh(fig3_1, use_container_width=True, key="plot3_1")
     
     st.divider()
 
     st.subheader("2. Year-over-Year Comparison of Death Causes")
-    fig3_2 = plot_year_comparison_barplot(data)
+    fig3_2 = plot_year_comparison_barplot(data_set3_cleaned)
     with st.container():
         streamlit_bokeh(fig3_2, use_container_width=True, key="plot3_2")
 
     st.divider()
     
     st.subheader("3. Infectious Disease Subgroups Over Time")
-    fig3_4 = plot_infectious_diseases(infectdata)
+    fig3_4 = plot_infectious_diseases(dataset3_infectdata)
     with st.container():
         streamlit_bokeh(fig3_4, use_container_width=True, key="plot3_4" )
